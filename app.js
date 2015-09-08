a765ac90dca4c9fef39bb545e0220c36b9169703
CCDice <support@apexhorizon.net>
2015-09-07 22:52:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LeefwwTAAAAAOGHkbwphs-oGdrIyGMH2C74Ei7Z',  // <----- EDIT ME!
-  redirect_uri: 'https://ccdice.net',
+  redirect_uri: 'http://ccdice.net',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
